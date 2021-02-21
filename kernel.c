34b7bb2c1eb6420d2ef0a7be9e8152cea80b7af1
Fadel <fadelananda123@gmail.com>
2021-02-21 20:39:05 +0700

    add kernel.c and add template from spek

---
+++ kernel.c
@@ -0,0 +1,17 @@
+/*
+Made by TubOS
+1. Pratama Andiko           13519112
+2. Fabian Savero Diaz P.    13519140
+3. Fadel Ananda D.          13519146
+*/
+
+void handleInterrupt21(int AX, int BX, int CX, int DX);
+void printString(char* string);
+void readString(char* string);
+void clear(char* buffer, int length);
+
+int main(){
+
+
+    while (1);
+}
