f852d149081b47cab22eacc11181267eb54278e9
fadelananda <fadelananda123@gmail.com>
2021-03-17 18:12:49 +0700

    finishing2

--- additional/stdfunc.c
+++ additional/stdfunc.c
@@ -35,28 +35,6 @@ int strlen(char* string){
     return count;
 }
 
-//copy str1 ke str2
-// void strcopy(char* str1, char* str2){
-
-//     while (*str1 != '\0')
-//     {
-//         *str2 = *str1;
-//         str1++;
-//         str2++;
-//     }
-//     *str2 = '\0';
-// }
-
-// void strncopy(char* src, char* dest, char x){
-
-//     int i = 0;
-//     while (src[i] != '\0' && src[i] != x) {
-//         dest[i] = src[i];
-//         i++;
-//     }
-//     dest[i] = '\0';
-// }
-
 void strcopy(char *src, char *dest) {
     int i = 0;
     while (src[i] != '\0') {
@@ -85,7 +63,7 @@ void strncpy(char *destination, char *source, int n){
     *destination = '\0';
 }
 
-void splitstring(char *src, char* part1, char* part2, char x) {
+void splitstring(char *src, char* part1, char *part2, char x) {
     int i = 0;
     int j = 0;
     while (src[i] != '\0' && src[i] != x) {
@@ -101,10 +79,6 @@ void splitstring(char *src, char* part1, char* part2, char x) {
     }
     part2[j] = '\0';
 }
-//a[3][5]
-//  11111 22222 33333
-//               a[2][1] = *(a+5i+j)
-// input [128][16];
 
 void copySegmentSectorSectors(char *dest, char *src, int n) {
     int i = 0;
--- additional/stdfunc.h
+++ additional/stdfunc.h
@@ -2,11 +2,9 @@ int strcmp(char *str1, char*str2);
 int strlen(char* string);
 int mod(int x, int y);
 int div(int x, int y);
-//void strncpy(char *destination, char *source, int n);
-//void strncopy(char* src, char* dest, char x);
+
 void splitstring(char *src, char* part1, char* part2, char x);
 void strcpybounded(char *dest, char *src, int n);
 void strncpy(char *destination, char *source, int n);
-// void splitsting(char *input, )
 void strcopy(char *src, char *dest);
 void copySegmentSectorSectors(char *dest, char *src, int n);
