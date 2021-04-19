3ea7cd0bb64cb616360a3a23dc0fd405b75f7e99
fadelananda <fadelananda123@gmail.com>
2021-04-19 22:59:02 +0700

    add shell script for external program

--- program/makeprogram.sh
+++ program/makeprogram.sh
@@ -1,1 +1,1 @@
-bcc -ansi -c -o tes.o tes.c && ld86 -o tes -d string.o tes.o interrupt.o
+bcc -ansi -c -o tes.o $1 && ld86 -o tes -d string.o tes.o interrupt.o
--- program/tes.c
+++ program/tes.c
@@ -1,7 +1,14 @@
 extern int interrupt (int number, int AX, int BX, int CX, int DX);
 int main(){
-    interrupt(0x21, 0, "\rcokkkkk\n", 0, 0);
-    interrupt(0x21, 0, "\rcokkkkk2\n", 0, 0);
-    while(1);
+    char input[1024];
+    interrupt(0x10, 0x0003, 0, 0, 0);
+    interrupt(0x21, 0, "\rShell\n\r", 0, 0);
+    while(1){
+        interrupt(0x21, 1, input, 0, 0);
+        // if(strcmp(input, "shell") == 0){
+        //     interrupt(0x21, 0, "\rShell\n\r", 0, 0);
+        // }
+        interrupt(0x21, 0, "\r\n\r", 0, 0);
+    }
 }
 
