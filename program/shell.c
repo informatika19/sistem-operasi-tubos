7fd0e22bd53b42a0d9f13fb2114e944cfeeb5b16
fadelananda <fadelananda123@gmail.com>
2021-04-20 01:45:09 +0700

    heehehehhe sudah malam

--- program/shell.c
+++ program/shell.c
@@ -3,6 +3,8 @@ extern int interrupt (int number, int AX, int BX, int CX, int DX);
 
 int main(){
     char input[1024];
+    char arg1[512];
+    char arg2[512];
     char filesSector[2][512]; //contains filesystem files
     char currentDir = 0xFF; //current direcory (default root)
 
@@ -13,9 +15,11 @@ int main(){
         clear(input, 128);
         getCurrentDir(filesSector, currentDir); //get current directory
         readString(input);
+        splitstring(input, arg1, arg2, ' ');
         if(strcmp(input, "ls") == 0){
             ls(filesSector, currentDir);
-            printString("\n\r");
+        } if(strcmp(arg1, "cat") == 0){
+            cat(arg2, currentDir);
         }
         printString("\r\n\r");
     }
